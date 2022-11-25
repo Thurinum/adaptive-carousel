9cbbd456d7e78d5ad665e6c53578101197171f46
Thurinum <m.gagnon941@gmail.com>
2022-11-24 20:10:29 -0500

    feat: Full TypeScript port of the 2020 lib

---
+++ index.js
@@ -0,0 +1,210 @@
+"use strict";
+class Carousel {
+    constructor(params) {
+        this.isFullscreen = false;
+        this.isSwitching = false;
+        this.currentIndex = -1;
+        this.itemSets = params.itemSets;
+        const element = document.querySelector(params.selector);
+        if (!element)
+            throw new Error(`Carousel element ${params.selector} not found!`);
+        this.carousel = element;
+        this.carousel.classList.add("slideshow");
+        for (const itemSet of this.itemSets) {
+            if (!itemSet.triggerSelector)
+                continue;
+            const trigger = document.querySelector(itemSet.triggerSelector);
+            if (!trigger)
+                throw new Error(`Carousel trigger ${itemSet.triggerSelector} not found!`);
+            trigger.addEventListener("click", this.show);
+        }
+    }
+    setItemSet(itemSetName) {
+        var _a, _b;
+        const set = this.getItemSet(itemSetName);
+        this.carousel.innerHTML = "";
+        this.currentItemSet = set;
+        let content = `
+			<span class="slideshow_close">X</span>
+			<span class="slideshow_title">Document viewer</span>
+			<span class="slideshow_resize" title="Toggle fullscreen">&#8690;</span>
+			<span class="slideshow_progress"></span>`;
+        if (set.items.length > 1)
+            content += `
+				<span class="slideshow_arrow slideshow_arrow_left">&lt;</span>
+				<span class="slideshow_arrow slideshow_arrow_right">&gt;</span>`;
+        for (let i = 0; i < set.items.length; i++) {
+            const item = set.items[i];
+            content += item.isPath
+                ? `<${item.tagName} class="slide slide${i}" src="${item.src}"></${item.tagName}>`
+                : `<${item.tagName} class="slide slide${i}">${item.src}</${item.tagName}>`;
+        }
+        this.carousel.innerHTML = content;
+        if (set.items.length <= 1)
+            return;
+        const leftArrow = this.carousel.querySelector(".slideshow_arrow_left");
+        const rightArrow = this.carousel.querySelector(".slideshow_arrow_right");
+        leftArrow.addEventListener("click", () => this.toPreviousItem());
+        rightArrow.addEventListener("click", () => this.toNextItem());
+        leftArrow.style.transform = "translate(0, -50%) scale(1)";
+        rightArrow.style.transform = "translate(0, -50%) scale(1)";
+        (_a = document.querySelector(".slideshow_resize")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", (e) => {
+            if (this.isFullscreen) {
+                this.carousel.style.width = "85vw";
+                this.carousel.style.height = "80vh";
+                e.currentTarget.innerHTML = "&#8690;";
+                this.isFullscreen = false;
+            }
+            else {
+                this.carousel.style.width = "100vw";
+                this.carousel.style.height = "100%";
+                e.currentTarget.innerHTML = "&#8689;";
+                this.isFullscreen = true;
+            }
+        });
+        (_b = document.querySelector(".slideshow_close")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", this.hide);
+        if (set.items.length > 0) {
+            this.currentIndex = set.startIndex;
+            this.setItem(set.items.length - 1);
+        }
+    }
+    setItem(oldIndex) {
+        if (!this.currentItemSet) {
+            throw new Error("No current item set!");
+            return;
+        }
+        let currentSlide = document.querySelector(`.slide${oldIndex}`);
+        let newSlide = document.querySelector(`.slide${this.currentIndex}`);
+        let title = document.querySelector(".slideshow_title");
+        let progress = document.querySelector(".slideshow_progress");
+        clearTimeout(this.titleTimeout);
+        newSlide.style.transition = "none";
+        newSlide.style.opacity = '0';
+        if (this.currentIndex - oldIndex > 0)
+            newSlide.style.transform = "translate(100%)";
+        else
+            newSlide.style.transform = "translate(-100%)";
+        newSlide.style.display = "block";
+        newSlide.style.transition = `transform 1s cubic-bezier(.68,-0.55,.27,1.55), opacity 1s cubic-bezier(.68,-0.55,.27,1.55)`;
+        setTimeout(function () {
+            newSlide.style.transform = "translate(0)";
+            newSlide.style.opacity = '1';
+        }, 25);
+        if (this.currentIndex - oldIndex < 0 && currentSlide !== newSlide)
+            currentSlide.style.transform = "translate(+100%)";
+        else
+            currentSlide.style.transform = "translate(-100%)";
+        const currentItem = this.currentItemSet.items[this.currentIndex];
+        if (currentItem.isPath)
+            title.innerHTML = currentItem.name.replace(/^.*[\\\/]/, '');
+        else
+            title.innerHTML = currentItem.name;
+        title.style.top = "0";
+        this.titleTimeout = setTimeout(function () {
+            title.style.top = "-47px";
+        }, 4500);
+        progress.innerHTML = `${this.currentIndex + 1}/${this.currentItemSet.items.length}`;
+        if (currentSlide !== newSlide)
+            setTimeout(() => {
+                currentSlide.style.display = "none";
+                this.isSwitching = false;
+            }, 1000);
+    }
+    toNextItem() {
+        if (!this.currentItemSet) {
+            throw new Error("No current item set!");
+            return;
+        }
+        if (this.isSwitching)
+            return;
+        this.isSwitching = true;
+        let oldIndex = this.currentIndex;
+        this.currentIndex--;
+        if (this.currentIndex < 0)
+            this.currentIndex = this.currentItemSet.items.length - 1;
+        this.setItem(oldIndex);
+    }
+    toPreviousItem() {
+        if (!this.currentItemSet) {
+            throw new Error("No current item set!");
+            return;
+        }
+        if (this.isSwitching)
+            return;
+        this.isSwitching = true;
+        let oldIndex = this.currentIndex;
+        this.currentIndex++;
+        if (this.currentIndex >= this.currentItemSet.items.length)
+            this.currentIndex = 0;
+        this.setItem(oldIndex);
+    }
+    show() {
+        if (!this.currentItemSet) {
+            throw new Error("No current item set!");
+            return;
+        }
+        let target = this.carousel;
+        target.style.display = "block";
+        setTimeout(function () {
+            target.style.opacity = "1";
+            target.style.transform = "translate(-50%, -50%) scale(1.0)";
+        }, 500);
+    }
+    hide() {
+        let target = this.carousel;
+        target.style.transform = "translate(-50%, -50%) scale(0.9)";
+        target.style.opacity = "0";
+        setTimeout(function () {
+            target.style.display = "none";
+        }, 500);
+    }
+    getItemSet(name) {
+        const itemSet = this.itemSets.find(x => x.name === name);
+        if (!itemSet)
+            throw new Error(`Carousel item set with name ${name} not found!`);
+        return itemSet;
+    }
+}
+class CarouselItemSet {
+    constructor(params) {
+        this.startIndex = 0;
+        this.name = params.name;
+        this.items = params.items;
+        this.triggerSelector = params.triggerSelector;
+        this.startIndex = params.startIndex;
+    }
+}
+class CarouselItem {
+    constructor(params) {
+        this.name = params.name;
+        this.tagName = params.tagName;
+        this.src = params.src;
+        this.isPath = params.isPath;
+    }
+}
+const carousel = new Carousel({
+    selector: '#carousel',
+    itemSets: [
+        new CarouselItemSet({
+            name: 'carousel1',
+            startIndex: 0,
+            items: [
+                new CarouselItem({
+                    name: 'image1',
+                    src: 'https://i.imgur.com/1ZQ3Z1K.png',
+                    isPath: true,
+                    tagName: 'img'
+                }),
+                new CarouselItem({
+                    name: 'image2',
+                    src: 'https://i.imgur.com/1ZQ3Z1K.png',
+                    isPath: true,
+                    tagName: 'img'
+                }),
+            ]
+        }),
+    ]
+});
+carousel.setItemSet('carousel1');
+carousel.show();
+//# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,48 +1,285 @@
-///////////////////////////////////////////////
-// COPYRIGHT (C) 2022 THURINUM               //
-// Customizable popup window/carousel system //
-///////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+// LICENSED UNDER GPLv3. MADE WITH <3 BY THURINUM. ;3  //
+// Customizable popup window/carousel system           //
+/////////////////////////////////////////////////////////
 
 // A set of items in the carousel
-interface ICarousel {
+interface CarouselParams {
 	selector: string
-	items: ICarouselItem[]
-	isFullscreen: boolean
-	titleTimeout: number
-	startIndex: number
-	triggerSelector: string
+	itemSets: CarouselItemSet[]
 }
-class Carousel implements ICarousel {
-	selector: string
-	items: ICarouselItem[]
-	isFullscreen: boolean
-	titleTimeout: number
-	startIndex: number
-	triggerSelector: string
+class Carousel {
+	carousel: HTMLElement
+	itemSets: CarouselItemSet[]
 
-	constructor(params: ICarousel) {
-		this.selector = params.selector
-		this.items = params.items
-		this.isFullscreen = params.isFullscreen
-		this.titleTimeout = params.titleTimeout
-		this.startIndex = params.startIndex
-		this.triggerSelector = params.triggerSelector
+	private titleTimeout?: any
+	private isFullscreen: boolean = false
+	private isSwitching: boolean = false
+	private currentIndex: number = -1
+	private currentItemSet?: CarouselItemSet
+
+	constructor(params: CarouselParams) {
+		this.itemSets = params.itemSets
+
+		// get the carousel element
+		const element = document.querySelector(params.selector);
+
+		if (!element)
+			throw new Error(`Carousel element ${params.selector} not found!`);
+
+		this.carousel = element as HTMLElement;
+		this.carousel.classList.add("slideshow");
+
+		// setup set triggers
+		for (const itemSet of this.itemSets) {
+			if (!itemSet.triggerSelector)
+				continue;
+
+			const trigger = document.querySelector(itemSet.triggerSelector);
+
+			if (!trigger)
+				throw new Error(`Carousel trigger ${itemSet.triggerSelector} not found!`);
+
+			trigger.addEventListener("click", this.show);
+		}
+	}
+
+	setItemSet(itemSetName: string) {
+		const set = this.getItemSet(itemSetName);
+
+		// remove all items from the carousel
+		this.carousel.innerHTML = "";
+		this.currentItemSet = set;
+
+		// add base controls
+		let content = `
+			<span class="slideshow_close">X</span>
+			<span class="slideshow_title">Document viewer</span>
+			<span class="slideshow_resize" title="Toggle fullscreen">&#8690;</span>
+			<span class="slideshow_progress"></span>`;
+
+		// show switching arrows when displaying multiple items
+		if (set.items.length > 1)
+			content += `
+				<span class="slideshow_arrow slideshow_arrow_left">&lt;</span>
+				<span class="slideshow_arrow slideshow_arrow_right">&gt;</span>`;
+
+		// add every item
+		for (let i = 0; i < set.items.length; i++) {
+			const item = set.items[i];
+
+			content += item.isPath
+				? `<${item.tagName} class="slide slide${i}" src="${item.src}"></${item.tagName}>`
+				: `<${item.tagName} class="slide slide${i}">${item.src}</${item.tagName}>`;
+		}
+
+		this.carousel.innerHTML = content;
+
+		// set event listeners for the controls
+		if (set.items.length <= 1)
+			return;
+
+		const leftArrow = this.carousel.querySelector(".slideshow_arrow_left") as HTMLElement;
+		const rightArrow = this.carousel.querySelector(".slideshow_arrow_right") as HTMLElement;
+
+		leftArrow.addEventListener("click", () => this.toPreviousItem());
+		rightArrow.addEventListener("click", () => this.toNextItem());
+		leftArrow.style.transform = "translate(0, -50%) scale(1)";
+		rightArrow.style.transform = "translate(0, -50%) scale(1)";
+
+		document.querySelector(".slideshow_resize")?.addEventListener("click", (e) => {
+			if (this.isFullscreen) {
+				this.carousel.style.width = "85vw";
+				this.carousel.style.height = "80vh";
+				(e.currentTarget as HTMLElement).innerHTML = "&#8690;"
+				this.isFullscreen = false;
+			} else {
+				this.carousel.style.width = "100vw";
+				this.carousel.style.height = "100%";
+				(e.currentTarget as HTMLElement).innerHTML = "&#8689;"
+				this.isFullscreen = true;
+			}
+		});
+
+		document.querySelector(".slideshow_close")?.addEventListener("click", this.hide);
+
+		// default to first item if there is one
+		if (set.items.length > 0) {
+			this.currentIndex = set.startIndex!;
+			this.setItem(set.items.length - 1);
+		}
+	}
+
+	setItem(oldIndex: number): void {
+		if (!this.currentItemSet) {
+			throw new Error("No current item set!");
+			return;
+		}
+
+		let currentSlide = document.querySelector(`.slide${oldIndex}`) as HTMLElement;
+		let newSlide = document.querySelector(`.slide${this.currentIndex}`) as HTMLElement;
+		let title = document.querySelector(".slideshow_title") as HTMLElement;
+		let progress = document.querySelector(".slideshow_progress") as HTMLElement;
+
+		// Clear old animation timer
+		clearTimeout(this.titleTimeout);
+
+		// Update the new slide
+		newSlide.style.transition = "none";
+		newSlide.style.opacity = '0';
+
+		if (this.currentIndex - oldIndex > 0)
+			newSlide.style.transform = "translate(100%)";
+		else
+			newSlide.style.transform = "translate(-100%)";
+
+		newSlide.style.display = "block";
+		newSlide.style.transition = `transform 1s cubic-bezier(.68,-0.55,.27,1.55), opacity 1s cubic-bezier(.68,-0.55,.27,1.55)`;
+
+		// Give time to the transition to update
+		setTimeout(function () {
+			newSlide.style.transform = "translate(0)";
+			newSlide.style.opacity = '1';
+		}, 25);
+
+		// Hide the old slide
+		if (this.currentIndex - oldIndex < 0 && currentSlide !== newSlide)
+			currentSlide.style.transform = "translate(+100%)";
+		else
+			currentSlide.style.transform = "translate(-100%)";
+
+		// Update slideshow progress
+		const currentItem = this.currentItemSet.items[this.currentIndex];
+
+		if (currentItem.isPath)
+			title.innerHTML = currentItem.name.replace(/^.*[\\\/]/, '');
+		else
+			title.innerHTML = currentItem.name
+
+		title.style.top = "0";
+		this.titleTimeout = setTimeout(function () {
+			title.style.top = "-47px";
+		}, 4500);
+		progress.innerHTML = `${this.currentIndex + 1}/${this.currentItemSet.items.length}`;
+
+		if (currentSlide !== newSlide)
+			setTimeout(() => {
+				currentSlide.style.display = "none";
+				this.isSwitching = false;
+			}, 1000);
+	}
+
+	toNextItem(): void {
+		if (!this.currentItemSet) {
+			throw new Error("No current item set!");
+			return;
+		}
+
+		// prevent switching item when already doing so
+		if (this.isSwitching)
+			return;
+
+		this.isSwitching = true;
+
+		let oldIndex = this.currentIndex;
+		this.currentIndex--;
+
+		// reset to opposite end if at the start
+		if (this.currentIndex < 0)
+			this.currentIndex = this.currentItemSet.items.length - 1;
+
+		this.setItem(oldIndex);
+	}
+
+	toPreviousItem(): void {
+		if (!this.currentItemSet) {
+			throw new Error("No current item set!");
+			return;
+		}
+
+		// prevent switching item when already doing so
+		if (this.isSwitching)
+			return;
+
+		this.isSwitching = true;
+
+		let oldIndex = this.currentIndex;
+		this.currentIndex++;
+
+		// reset to opposite end if at the end
+		if (this.currentIndex >= this.currentItemSet.items.length)
+			this.currentIndex = 0;
+
+		this.setItem(oldIndex);
+	}
+
+	show() {
+		if (!this.currentItemSet) {
+			throw new Error("No current item set!");
+			return;
+		}
+
+		let target = this.carousel;
+		target.style.display = "block";
+		setTimeout(function () {
+			target.style.opacity = "1";
+			target.style.transform = "translate(-50%, -50%) scale(1.0)";
+		}, 500);
+	}
+
+	hide(): void {
+		let target = this.carousel;
+		target.style.transform = "translate(-50%, -50%) scale(0.9)";
+		target.style.opacity = "0";
+
+		setTimeout(function () {
+			target.style.display = "none";
+		}, 500);
+	}
+
+	private getItemSet(name: string): CarouselItemSet {
+		const itemSet = this.itemSets.find(x => x.name === name);
+
+		if (!itemSet)
+			throw new Error(`Carousel item set with name ${name} not found!`);
+
+		return itemSet;
 	}
 }
 
-interface ICarouselItem {
+interface CarouselItemSetParams {
+	name: string
+	items: CarouselItem[]
+	triggerSelector?: string
+	startIndex?: number
+}
+class CarouselItemSet {
+	name: string
+	items: CarouselItem[]
+	triggerSelector?: string
+	startIndex?: number = 0 // TODO: Unused
+
+	constructor(params: CarouselItemSetParams) {
+		this.name = params.name
+		this.items = params.items
+		this.triggerSelector = params.triggerSelector
+		this.startIndex = params.startIndex
+	}
+}
+
+interface CarouselItemParams {
 	name: string
 	src: string
 	isPath: boolean
 	tagName: string
 }
-class CarouselItem implements ICarouselItem {
+class CarouselItem {
 	name: string
 	src: string
 	isPath: boolean
 	tagName: string
 
-	constructor(public params: ICarouselItem) {
+	constructor(params: CarouselItemParams) {
 		this.name = params.name;
 		this.tagName = params.tagName;
 		this.src = params.src;
@@ -50,18 +287,30 @@ class CarouselItem implements ICarouselItem {
 	}
 }
 
-const carousel: Carousel = new Carousel({
-	selector: '.carousel',
-	items: [
-		new CarouselItem({
-			name: 'Image 1',
-			src: 'https://i.imgur.com/1JZ9Q2r.png',
-			isPath: false,
-			tagName: 'img'
+// instantiate the carousel (TEST)
+const carousel = new Carousel({
+	selector: '#carousel',
+	itemSets: [
+		new CarouselItemSet({
+			name: 'carousel1',
+			startIndex: 0,
+			items: [
+				new CarouselItem({
+					name: 'image1',
+					src: 'https://i.imgur.com/1ZQ3Z1K.png',
+					isPath: true,
+					tagName: 'img'
+				}),
+				new CarouselItem({
+					name: 'image2',
+					src: 'https://i.imgur.com/1ZQ3Z1K.png',
+					isPath: true,
+					tagName: 'img'
+				}),
+			]
 		}),
-	],
-	isFullscreen: true,
-	titleTimeout: 5000,
-	startIndex: 0,
-	triggerSelector: ".carousel-trigger"
+	]
 })
+
+carousel.setItemSet('carousel1');
+carousel.show();
